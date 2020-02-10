920a874e7d5391ea02e54d29e3df62cc9a6d4660
Maxim Lyapin <maxim@mlyap.in>
2020-02-10 15:39:33 +1100

    Add poor-poor assert.h implementation

---
+++ libc/include/assert.h
@@ -0,0 +1,11 @@
+#undef assert
+
+#ifdef NDEBUG
+#define assert(exp) ((void)0)
+#else
+// TODO(Maxim Lyapin): Well. Right now assertion isn't very good. Make it good.
+#define assert(exp) ((exp) && ((*(int*)0x0 = 0), 1))
+#endif
+
+// TODO(Maxim Lyapin): Mark as C11 or newer only
+#define static_assert(exp, str) _Static_assert(exp, str)
