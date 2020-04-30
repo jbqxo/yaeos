11832be26114d3af85b2171d48aebadabbe1d75c
Maxim Lyapin <maxim@mlyap.in>
2020-04-30 23:14:32 +1100

    [libc] Add memset tests

---
+++ libc/lib/string/memset_tests.c
@@ -0,0 +1,53 @@
+#include "unity/src/unity.h"
+#include <stdbool.h>
+#include <unity.h>
+#include <string.h>
+#include <stddef.h>
+
+#define GUARDVAL 0xAAu
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+static void memset_simple(void)
+{
+	const size_t length = 64;
+	const unsigned char val = 'A';
+
+	unsigned char dest[length + 32];
+	for (size_t i = 0; i < 32; i++) {
+		dest[length + i] = GUARDVAL;
+	}
+
+	void *res = memset(dest, val, length);
+	TEST_ASSERT_EQUAL_PTR(dest, res);
+	TEST_ASSERT_EACH_EQUAL_CHAR(val, dest, length);
+	TEST_ASSERT_EACH_EQUAL_CHAR_MESSAGE(GUARDVAL, &dest[length], 32,
+					    "Guard value is ruined");
+}
+
+static void memset_empty_str(void)
+{
+	unsigned char dest[32];
+	for (size_t i = 0; i < sizeof(dest); i++) {
+		dest[i] = GUARDVAL;
+	}
+
+	void *res = memset(dest, 'A', 0);
+	TEST_ASSERT_EQUAL_PTR(dest, res);
+	TEST_ASSERT_EACH_EQUAL_CHAR_MESSAGE(GUARDVAL, dest, 32,
+					    "Guard value is ruined");
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(memset_simple);
+	RUN_TEST(memset_empty_str);
+	return UNITY_END();
+}
