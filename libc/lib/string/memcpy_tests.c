703b5dc5a018aa61f2700861c1ecee751960036f
Maxim Lyapin <maxim@mlyap.in>
2020-04-30 23:14:03 +1100

    [libc] Add memcpy tests

---
+++ libc/lib/string/memcpy_tests.c
@@ -0,0 +1,52 @@
+#include <stdbool.h>
+#include <unity.h>
+#include <string.h>
+#include <stddef.h>
+
+#define GUARDVAL 0xAAu
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+static void memcpy_simple(void)
+{
+	static const unsigned char src[] = "Test string";
+	unsigned char dest[sizeof(src) + 32];
+	for (size_t i = 0; i < 32; i++) {
+		dest[sizeof(src) + i] = GUARDVAL;
+	}
+
+	void *res = memcpy(dest, src, sizeof(src));
+	TEST_ASSERT_EQUAL_PTR(dest, res);
+	TEST_ASSERT_EQUAL_STRING(src, dest);
+	TEST_ASSERT_EACH_EQUAL_CHAR_MESSAGE(GUARDVAL, &dest[sizeof(src)], 32,
+					    "Guard value is ruined");
+}
+
+static void memcpy_empty_str(void)
+{
+	static const unsigned char src[] = "";
+	unsigned char dest[sizeof(src) + 32];
+	for (size_t i = 0; i < 32; i++) {
+		dest[sizeof(src) + i] = GUARDVAL;
+	}
+
+	void *res = memcpy(dest, src, sizeof(src));
+	TEST_ASSERT_EQUAL_PTR(dest, res);
+	TEST_ASSERT_EQUAL_STRING(src, dest);
+	TEST_ASSERT_EACH_EQUAL_CHAR_MESSAGE(GUARDVAL, &dest[sizeof(src)], 32,
+					    "Guard value is ruined");
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(memcpy_simple);
+	RUN_TEST(memcpy_empty_str);
+	return UNITY_END();
+}
