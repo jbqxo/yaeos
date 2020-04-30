b5b2f823ca2e355c939ae16fcb3c28dd6b642008
Maxim Lyapin <maxim@mlyap.in>
2020-04-30 23:16:09 +1100

    [libc] Change memset implentation a bit

--- libc/lib/string/memset.c
+++ libc/lib/string/memset.c
@@ -2,10 +2,9 @@
 #include <stdint.h>
 
 void *memset(void *_data, int val, size_t len) {
-    // TODO: There is a vast room for optimizations.
     unsigned char *data = _data;
-    for(;len; len--) {
-        data[len] = (uint8_t)val;
+    for(size_t i = 0; i < len; i++) {
+        data[i] = (uint8_t)val;
     }
     return data;
 }
