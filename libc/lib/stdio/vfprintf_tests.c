59d37dea26cf825ed154165cd29603aae4cd74aa
Maxim Lyapin <maxim@mlyap.in>
2020-04-30 23:03:37 +1100

    [libc] Improve vfprintf unit-tests

--- libc/lib/stdio/vfprintf_tests.c
+++ libc/lib/stdio/vfprintf_tests.c
@@ -14,10 +14,15 @@ void tty_putchar(tty_descriptor_t d, char c)
 	buffer[pos++] = c;
 }
 
-void setUp(void)
+static void reset(void)
 {
 	memset(buffer, GUARDVAL, sizeof(buffer));
 	pos = 0;
+}
+
+void setUp(void)
+{
+	reset();
 }
 
 void tearDown(void)
@@ -27,76 +32,106 @@ void tearDown(void)
 #define VFPRINTF(expected, format, ...)                                        \
 	do {                                                                   \
 		int s = strlen(expected);                                      \
-		TEST_ASSERT_EQUAL_INT(s, fprintf(0, (format), ##__VA_ARGS__));  \
+		int r = fprintf(0, (format), ##__VA_ARGS__);                   \
 		TEST_ASSERT_EQUAL_HEX8(GUARDVAL, buffer[s]);                   \
 		buffer[s] = '\0';                                              \
-		TEST_ASSERT_EQUAL_STRING((expected), buffer);                    \
-		setUp();                                                       \
+		TEST_ASSERT_EQUAL_STRING((expected), buffer);                  \
+		TEST_ASSERT_EQUAL_INT(s, r);                                   \
+		reset();                                                       \
 	} while (0);
 
-static void vfprintf_simple(void)
+static void simple(void)
 {
 	VFPRINTF("16", "16");
+}
+
+static void conv_int(void)
+{
 	VFPRINTF("2 + 14 = 16", "2 + 14 = %d", 16);
 	VFPRINTF("2 + 14 = -16", "2 + 14 = %d", -16);
-	VFPRINTF("Hello world!", "Hello %s!", "world");
-}
+	VFPRINTF("2 + 14 = 16", "2 + 14 = %i", 16);
+	VFPRINTF("2 + 14 = -16", "2 + 14 = %i", -16);
 
-// TODO: It would be cool to cover whole set of behaviour.
-static void vfprintf_flag_minus(void)
-{
+	// Width
+	VFPRINTF("  16", "%4d", 16);
+
+	// Precision
+	VFPRINTF("16", "%.0d", 16);
+	VFPRINTF("", "%.0d", 0);
+	VFPRINTF("0", "%.1d", 0);
+
+	// Length: hh
+	VFPRINTF("-128", "%hhd", 0x7F + 1);
+
+	// Length: h
+	VFPRINTF("-32768", "%hd", 0x7FFF + 1);
+
+	// TODO: Test all length modifiers
+
+	// Flag: -
 	VFPRINTF("16 ", "%-3d", 16);
-	VFPRINTF(" 16", "%3d", 16);
-}
 
-static void vfprintf_flag_plus(void)
-{
+	// Flag: +
 	VFPRINTF("+16", "%+d", 16);
 	VFPRINTF("-16", "%+d", -16);
 	VFPRINTF("16", "%d", 16);
 	VFPRINTF("-16", "%d", -16);
-}
 
-static void vfprintf_flag_space(void)
-{
+	// Flag: _space_
 	VFPRINTF(" 16", "% d", 16);
+	VFPRINTF("-16", "% d", -16);
 	VFPRINTF("+16", "% +d", 16);
 	VFPRINTF("-16", "% +d", -16);
-}
 
-static void vfprintf_flag_hash(void)
-{
-	VFPRINTF("0x10", "%#x", 16);
-	VFPRINTF(" 0x10", "%#x", 16);
-}
-
-static void vfprintf_flag_zero(void)
-{
+	// Flag: 0
 	VFPRINTF("0016", "%04d", 16);
 	VFPRINTF("-016", "%04d", -16);
 }
 
-static void vfprintf_conv_spec_int(void)
+static void conv_str(void)
 {
-	VFPRINTF("16", "%d", 16);
-	VFPRINTF("16", "%.0d", 16);
-	VFPRINTF("", "%.0d", 0);
-	VFPRINTF("0", "%.1d", 0);
+	VFPRINTF("Test string!", "Test %s!", "string");
+
+	// Precision
+	VFPRINTF("Test str!", "Test %.3s!", "string");
+	VFPRINTF("Test string!", "Test %.10s!", "string");
+
+	// TODO: Wide characters
+}
+
+static void conv_ptr(void)
+{
+	void *ptr = (uintptr_t)0xABC123;
+
+	VFPRINTF("0xABC123", "%p", ptr);
+	VFPRINTF("  0xABC123", "%10p", ptr);
+	VFPRINTF("0xABC123  ", "%-10p", ptr);
+}
+
+static void conv_uhex(void)
+{
+}
+static void conv_uoctal(void)
+{
+}
+static void conv_udec(void)
+{
+}
+static void conv_uchar(void)
+{
+}
+static void conv_written_output(void)
+{
+}
+static void conv_percentage(void)
+{
 }
 
 int main(void)
 {
 	UNITY_BEGIN();
 
-	RUN_TEST(vfprintf_simple);
-
-	RUN_TEST(vfprintf_flag_minus);
-	RUN_TEST(vfprintf_flag_plus);
-	RUN_TEST(vfprintf_flag_space);
-	RUN_TEST(vfprintf_flag_hash);
-	RUN_TEST(vfprintf_flag_zero);
-
-	RUN_TEST(vfprintf_conv_spec_int);
+	RUN_TEST(simple);
 
 	return UNITY_END();
 }
