9c3cc06bc09c6c41221daea234f475d9fcff0129
Maxim Lyapin <maxim@mlyap.in>
2020-05-01 22:44:39 +1100

    Yet again, some improvements to the build system

--- conf.mk
+++ conf.mk
@@ -6,13 +6,13 @@ endif
 # We need to export it in order to save the status for embedded(?) make calls.
 
 ifeq ($(findstring test,$(MAKECMDGOALS)),)
-    export BUILD_TEST ?= 0
+    export BUILD_TEST ?=
 else
     export BUILD_TEST ?= 1
 endif
 
 ifeq ($(findstring 1,$(NDEBUG)),1)
-    export BUILD_DEBUG ?= 0
+    export BUILD_DEBUG ?=
 else
     export BUILD_DEBUG ?= 1
 endif
@@ -40,13 +40,13 @@ BUILDDIR_DEPS   ?= $(BUILDDIR_BUILD)/$(DIR_DEPS)
 # TODO: Make -mgeneral-regs-only enabled only for the arch-specific code, when
 # floating point, sse, mmx, sse2, 3dnow and avx instructions will be available.
 CFLAGS_DEBUG     := -O0 -g
-CFLAGS_RELEASE   := -O2
+CFLAGS_RELEASE   := -O3
 CFLAGS_COMMON    := -std=gnu18 -mgeneral-regs-only
 
 CPPFLAGS_DEBUG   :=
 CPPFLAGS_RELEASE := -DNDEBUG
 
-LDFLAGS_COMMON  :=
+LDFLAGS_COMMON  := -lgcc
 LDFLAGS_DEBUG   := -fno-lto
 LDFLAGS_RELEASE := -flto
 
