0af9c9a7829df4aabf8579bb3dd4f988890da10c
Maxim Lyapin <maximl@mm.st>
2020-09-02 14:35:17 +1100

    Add strcmp implementation

---
+++ kernel/lib/string/strcmp.c
@@ -0,0 +1,24 @@
+#include "lib/string.h"
+
+int strcmp(const char *s1, const char *s2) {
+	const unsigned char *lhs = (const unsigned char*)s1;
+	const unsigned char *rhs = (const unsigned char*)s2;
+	while (lhs && rhs && *lhs == *rhs) {
+		lhs++;
+		rhs++;
+	}
+
+	return (*lhs - *rhs);
+}
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+	const unsigned char *lhs = (const unsigned char*)s1;
+	const unsigned char *rhs = (const unsigned char*)s2;
+	while (n > 0 && lhs && rhs && *lhs == *rhs) {
+		lhs++;
+		rhs++;
+		n--;
+	}
+
+	return (*lhs - *rhs);
+}
