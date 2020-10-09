b918667aa70e030bff303c681eb0505b9e410f35
Maxim Lyapin <maximl@mm.st>
2020-10-10 06:16:27 +1100

    Make foreach_macroses.sh use grep instead of git grep

--- scripts/foreach_macroses.sh
+++ scripts/foreach_macroses.sh
@@ -2,4 +2,4 @@
 
 # List of FOREACH macroses
 
-git grep -h '^#define [^[:space:]]*FOREACH[^[:space:]]*(' HEAD  | sed "s,^#define \([^[:space:]]*FOREACH[^[:space:]]*\)(.*$,  - '\1'," | sort | uniq
+grep -RI -h '^#define [^[:space:]]*FOREACH[^[:space:]]*(' . | sed "s/^#define \([^[:space:]]*FOREACH[^[:space:]]*\)(.*$/  - '\1'/" | sort | uniq
